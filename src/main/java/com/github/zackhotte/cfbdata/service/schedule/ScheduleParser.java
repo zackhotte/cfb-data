90fba7c90b62000137778e44847b48434a843fb0
zackhot <zack.hotte@gmail.com>
2018-08-05 22:21:42 -0400

    Implemented CFBParser on schedule service

--- src/main/java/com/github/zackhotte/cfbdata/service/schedule/ScheduleParser.java
+++ src/main/java/com/github/zackhotte/cfbdata/service/schedule/ScheduleParser.java
@@ -1,42 +1,42 @@
 package com.github.zackhotte.cfbdata.service.schedule;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.zackhotte.cfbdata.service.CFBParser;
 import lombok.Getter;
 
-import java.io.IOException;
-import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 @Getter
-public class ScheduleParser {
+public class ScheduleParser extends CFBParser<Schedule> {
 
     private JsonNode scheduleNode;
 
     private long season;
     private int week;
-    private List<String> dates;
+    private List<String> dates = new ArrayList<>();
     private Map<String, Schedule> schedules = new HashMap<>();
 
-    public ScheduleParser(int season, int week) {
-        try {
-            this.season = season;
-            this.week = week;
-            String base_url = String.format("http://cdn.espn.com/core/college-football/schedule?year=%s&week=%s&xhr=1", season, week);
-            URL url = new URL(base_url);
-            ObjectMapper mapper = new ObjectMapper();
-            this.scheduleNode = mapper.readTree(url).get("content").get("schedule");
-            getAllDatesInWeek();
-            getAllScheduledGames();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public ScheduleParser(Integer season, Integer week) {
+        super("http://cdn.espn.com/core/college-football/schedule?year=%s&week=%s&xhr=1", season, week);
+        this.season = season;
+        this.week = week;
+        this.scheduleNode = this.root.get("content").get("schedule");
+        getAllDatesInWeek();
+        getAllScheduledGames();
+    }
+
+    @Override
+    public List<Schedule> getData() {
+        return new ArrayList<>(schedules.values());
     }
 
     private void getAllDatesInWeek() {
-        dates = new ArrayList<>();
         for (Iterator<String> it = scheduleNode.fieldNames(); it.hasNext(); ) {
             String dateStr = it.next();
             dates.add(dateStr);
@@ -47,7 +47,6 @@ public class ScheduleParser {
         DateTimeFormatter dateTimeFormatter;
         if (dateStr.contains("Z") || dateStr.contains("T")) {
             dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm'Z'");
-            // "2017-10-20T00:00Z",
         } else {
             dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
         }
@@ -62,7 +61,6 @@ public class ScheduleParser {
             nodes.forEach(this::setSchedule);
         });
     }
-
 
     private void setSchedule(JsonNode node) {
         Schedule schedule = new Schedule();
